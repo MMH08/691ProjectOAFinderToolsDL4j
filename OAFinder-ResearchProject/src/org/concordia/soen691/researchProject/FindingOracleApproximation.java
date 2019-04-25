70990954047ca0f17e0ebd0348993c3943c54082
Manik Hossain <45977153+MMH08@users.noreply.github.com>
2019-04-25 12:07:29 -0400

    removing some unwanted function which looks like Oracle Approximation functions
    removing some unwanted function which looks like Oracle Approximation functions

--- OAFinder-ResearchProject/src/org/concordia/soen691/researchProject/FindingOracleApproximation.java
+++ OAFinder-ResearchProject/src/org/concordia/soen691/researchProject/FindingOracleApproximation.java
@@ -16,12 +16,44 @@ public class FindingOracleApproximation extends VoidVisitorAdapter<ArrayList<Str
 	public void visit(MethodCallExpr OAMethodExpr, ArrayList<String> OAcollector) {
 
 		String[] functionFormats = {">","<",">=","<="};
+		String getType = "";
+		boolean addToList = false;
 		if(OAMethodExpr.toString().contains("assert") && OAMethodExpr.getArguments().size() >= 3) {
+
 			boolean isOA = checkOracleApproximationFunction(OAMethodExpr.getNameAsString().toString()); 
 			if(isOA) {
-			OAcollector.add(OAMethodExpr.getNameAsString().toString() +","+ OAMethodExpr.getBegin().get().line);
-			}
+				
+				if(OAMethodExpr.toString().contains("assert") && OAMethodExpr.getArguments().size() == 3) {
+					try {
+					 // removing some unwanted function which looks like Oracle Approximation functions 	
+					 getType = OAMethodExpr.getArgument(0).calculateResolvedType().describe();
+					 if(!getType.contains("String")) {
+						 addToList = true;
+						 }
+					 else addToList = false;
+					 
+					}catch (Exception e) {
+						// removing some unwanted function which looks like Oracle Approximation functions 
+						if(OAMethodExpr.getArgument(0).toString().contains("getCount")) {
+							addToList = false;
+						}
+						else addToList=true;
+					}
+					
+					if(addToList) {
+						OAcollector.add(OAMethodExpr.getNameAsString().toString() +","+ OAMethodExpr.getBegin().get().line);
+						}
+					
+				}
+				// if it contains more than 3 Arguments, it certainly Oracle approximation with proper DELTA value 
+				else if(OAMethodExpr.toString().contains("assert") && OAMethodExpr.getArguments().size() > 3) {
+					OAcollector.add(OAMethodExpr.getNameAsString().toString() +","+ OAMethodExpr.getBegin().get().line);
+				}
+			 }
+			
 		}
+		//Handle assertTrue differently because this functions contains one Argument and compare the value with constant 
+		//value using this expression ">","<",">=","<="
 		else if(  OAMethodExpr.toString().contains("assertTrue")) {
 			for (String s : functionFormats)
 			{
@@ -34,11 +66,11 @@ public class FindingOracleApproximation extends VoidVisitorAdapter<ArrayList<Str
 		}
 	}
 	
-	//Remove some unwanted OA
+	//Removing some unwanted functions which looks like Oracle Approximations
 	private boolean checkOracleApproximationFunction(String functionsName) {
 			boolean flag = false; 
 			String[] unwantedFunctions = {"assertCharacterCategories","assertExpectedNumberMachineIdsJvmIdsThreadIds","assertMultiThreadedTokenizedStreamEquals",
-					"assertNInNOutSet","assertTokenizedStreamEquals"};
+					"assertNInNOutSet","assertTokenizedStreamEquals","assertSupported"};
 			if((Arrays.asList(unwantedFunctions).contains(functionsName))) {
 				flag = false;
 			}
